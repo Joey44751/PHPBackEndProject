7e5823ac9df387231a2b64334074835769a060b3
adventuresmurf <lemmensn@hotmail.com>
2021-05-27 11:04:57 +0200

    Models toegevoegd + relaties aangelegd

---
+++ backend/app/Models/companies.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class companies extends Model
+{
+    use HasFactory;
+
+public function size(){
+    return $this->hasone(Size::class);
+}
+
+public function price(){
+    return $this->hasone(Price::class);
+}
+    
+}
