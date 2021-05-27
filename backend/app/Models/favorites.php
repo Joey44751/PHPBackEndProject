33fd6cf0c76ce98365c0d7d448f6c96bb0fbeabf
adventuresmurf <lemmensn@hotmail.com>
2021-05-27 12:35:17 +0200

    added favorites model

---
+++ backend/app/Models/favorites.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class favorites extends Model
+{
+    use HasFactory;
+
+    public function company(){
+        return $this->hasMany(Company::class);
+    }
+    
+    public function user(){
+        return $this->hasone(User::class);
+    }
+}
